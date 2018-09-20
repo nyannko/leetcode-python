2e68c9aeb7165ed1068eb3911f7229a9cdfec4e7
nyannko <9638293+nyannko@users.noreply.github.com>
2018-09-20 23:18:53 +0200

    20/09/2018,23:18

---
+++ alg/plus_one.py
@@ -0,0 +1,18 @@
+class Solution:
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        num = str(int(''.join(map(str, digits))) + 1)
+        return [int(s) for s in num]
+
+    def plusOne1(self, digits):
+        num = 0
+        for i, v in enumerate(digits):
+            print((len(digits) - 1 - i))
+            num += v * pow(10, (len(digits) - 1 - i))
+        return [int(i) for i in str(num + 1)]
+
+a = Solution()
+print(a.plusOne1([4,3,2,1]))
