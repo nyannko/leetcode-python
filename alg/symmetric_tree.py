a3bc92fccb40c056dda7c1ebe7a5bf4406b5e3b4
nyannko <9638293+nyannko@users.noreply.github.com>
2018-09-27 21:43:09 +0200

    27/09/2018,21:43

---
+++ alg/symmetric_tree.py
@@ -0,0 +1,41 @@
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if not root:
+            return True
+        else:
+            return self.dfs(root.left, root.right)
+
+    def dfs(self, left, right):
+        if not left and not right:
+            return True
+        if not left or not right:
+            return False
+        if left.val == right.val:
+            return self.dfs(left.left, right.right) and self.dfs(left.right, right.left)
+        else:
+            return False
+
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        if not p and not q:
+            return True
+        if not q or not p:
+            return False
+        return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
