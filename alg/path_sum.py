ee52e7c56732f589c3c5b3133b542cc61bc64e14
nyannko <9638293+nyannko@users.noreply.github.com>
2018-09-28 23:56:47 +0200

    28/09/2018,23:56

--- alg/path_sum.py
+++ alg/path_sum.py
@@ -12,3 +12,29 @@ class Solution:
 
         return self.hasPathSum(root.left, sum - root.val) or \
                self.hasPathSum(root.right, sum - root.val)
+
+    class Solution(object):
+        def pathSum(self, root, sum):
+            """
+            :type root: TreeNode
+            :type sum: int
+            :rtype: List[List[int]]
+            """
+            if not root: return []
+            res = []
+            path = []
+            self.dfs(sum, root, path, res)
+            return res
+
+        def dfs(self, sum, root, path, res):
+            if sum == root.val and not root.left and not root.right:
+                path.append(root.val)
+                # print(path)
+                res.append(path)
+                return
+
+            # print(path)
+            if root.left:
+                self.dfs(sum - root.val, root.left, list(path) + [root.val], res)
+            if root.right:
+                self.dfs(sum - root.val, root.right, list(path) + [root.val], res)
