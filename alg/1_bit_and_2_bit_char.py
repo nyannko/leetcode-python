36c57e795d1cf3555cd3dba188acbf2f16e61fbe
nyannko <9638293+nyannko@users.noreply.github.com>
2018-10-18 23:57:09 +0200

    18/10/2018,23:57

---
+++ alg/1_bit_and_2_bit_char.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isOneBitCharacter(self, bits):
+        """
+        :type bits: List[int]
+        :rtype: bool
+        """
+        count = 0
+        for i in range(len(bits) - 2, -1, -1):
+            if bits[i] == 1:
+                count += 1
+            else:
+                break
+        if count & 1 == 1:
+            return False
+        else:
+            return True
