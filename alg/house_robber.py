f9e6ffd3dc9934b07ad6b0b4739f87d6bacb92c3
nyannko <9638293+nyannko@users.noreply.github.com>
2018-10-20 23:55:18 +0200

    20/10/2018,23:55

--- alg/house_robber.py
+++ alg/house_robber.py
@@ -7,7 +7,14 @@ class Solution(object):
         odd = even = 0
         for i in range(len(nums)):
             if i & 1 == 0:
+                print("1", odd, even)
                 odd = max(odd + nums[i], even)
+                print("2", odd, even)
             else:
+                print("3", odd, even)
                 even = max(even + nums[i], odd)
+                print("4", odd, even)
         return max(odd, even)
+
+a = Solution()
+a.rob([2,1,1,2])
