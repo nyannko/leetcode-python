39ce7c5fdd098fbc0bb0a75eaca354531f2c22ef
nyannko <9638293+nyannko@users.noreply.github.com>
2019-01-09 21:18:54 +0100

    09/01/2019,21:18

---
+++ alg/merge_k_sorted_list.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    # naive
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        while len(lists) >= 2:
+            new_list = self.merge2_lists(lists[-1], lists[-2])
+            lists.insert(0, new_list)
+            del lists[-2:]  # del the last teo element
+        if len(lists) == 1:
+            return lists[0]
+        return []
+
+    def merge2_lists(self, l1, l2):
+        p = ListNode(0)
+        head = p
+        while l1 and l2:
+            if l1.val < l2.val:
+                p.next = ListNode(l1.val)
+                l1 = l1.next
+                p = p.next
+            else:
+                p.next = ListNode(l2.val)
+                l2 = l2.next
+                p = p.next
+
+        while l1:
+            p.next = ListNode(l1.val)
+            l1 = l1.next
+            p = p.next
+
+        while l2:
+            p.next = ListNode(l2.val)
+            l2 = l2.next
+            p = p.next
+        return head.next
