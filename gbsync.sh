5574434948c81e41c0220e92737440826df5a9a4
nyannko <9638293+nyannko@users.noreply.github.com>
2019-01-14 22:21:01 +0100

    update .gitignore

--- gbsync.sh
+++
@@ -1,9 +0,0 @@
-gitbook build
-cd $HOME/github/leetcode-python/
-git co gh-pages 
-cp -r $HOME/github/gitbooks/leetcode/_book/* $HOME/github/leetcode-python/
-git add .
-dt=$(date '+%d/%m/%Y,%H:%M')
-git commit -m $dt
-git push -u origin gh-pages
-git co master
