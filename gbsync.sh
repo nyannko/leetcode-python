6ad05fe866ac94778fc69d1597d208ecadcf686d
nyannko <9638293+nyannko@users.noreply.github.com>
2019-04-11 01:28:32 +0200

    11/04/2019,01:28

--- gbsync.sh
+++ gbsync.sh
@@ -5,6 +5,13 @@ git co gh-pages
 cp -r $HOME/github/gitbooks/leetcode/_book/* $HOME/github/leetcode-python/
 git add .
 dt=$(date '+%d/%m/%Y,%H:%M')
-git commit -m $dt
+git commit -m ${dt}
+# comsg=$@
+# echo ${comsg}
+# if [[ $# -eq 0 ]]; then # if no commit msg
+#     git commit -m ${dt}
+# else 
+#     git commit -m ${comsg}
+# fi
 git push -u origin gh-pages
 git co master
